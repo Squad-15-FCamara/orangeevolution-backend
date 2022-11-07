eb851a7880f050b89d09bfb57c54924932f7825a
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-07 12:52:41 -0300

    fix: courses repository add

---
+++ src/main/java/com/orange_evolution_backend/Repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package com.orange_evolution_backend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<CourseRepository,Long> {
+    
+}
