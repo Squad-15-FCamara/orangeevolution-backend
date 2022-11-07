1e496a1d893779355168acf6a58e7de46eecb122
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-07 13:27:02 -0300

    save course service add

--- src/main/java/com/orange_evolution_backend/Service/CourseService.java
+++ src/main/java/com/orange_evolution_backend/Service/CourseService.java
@@ -3,29 +3,30 @@ package com.orange_evolution_backend.Service;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.orange_evolution_backend.Entity.Course;
 import com.orange_evolution_backend.Repository.CourseRepository;
 
-
 import lombok.AllArgsConstructor;
-
 
 @Service
 @AllArgsConstructor
 public class CourseService {
     CourseRepository courseRepository;
 
-    public List<Course> findAllCourses(){
+    public List<Course> findAllCourses() {
         return courseRepository.findAll();
     }
-    
-    public Course findCourseByID(Long idCourse){
-        return courseRepository.findById(idCourse).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Este material não existe"));
+
+    public Course findCourseByID(Long idCourse) {
+        return courseRepository.findById(idCourse)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Este material não existe"));
     }
 
-
+    public Course saveCourse(Course course) {
+        courseRepository.save(course);
+        return course;
+    }
 }
