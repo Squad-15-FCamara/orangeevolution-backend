8d6af599fadfcde01c9f17dde170325d904b9f61
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-07 13:29:59 -0300

    feat: update user endpoint

--- src/main/java/com/orange_evolution_backend/controller/UserController.java
+++ src/main/java/com/orange_evolution_backend/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,18 @@ public class UserController {
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 		
 		return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User user) {
+		if (!userRepository.existsById(userId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		user.setId(userId);
+		user = userService.saveUser(user);
+		
+		return ResponseEntity.ok(user);
 	}
 	
 	@DeleteMapping("/{userId}")
