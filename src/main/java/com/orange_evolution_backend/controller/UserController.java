63802a7d8c28312a64e08ebcccb7a7e566824ffd
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-07 13:14:22 -0300

    feat: post request for saving an user

--- src/main/java/com/orange_evolution_backend/controller/UserController.java
+++ src/main/java/com/orange_evolution_backend/controller/UserController.java
@@ -2,10 +2,13 @@ package com.orange_evolution_backend.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,12 @@ public class UserController {
 	public ResponseEntity<User> getUserById(@PathVariable Long userId) {
 		
 		return ResponseEntity.ok(userService.findUserById(userId));
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		
+		return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.CREATED);
 	}
 	
 }
