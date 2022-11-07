635061139bf3e79d717f5e73fee60578588be504
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-07 15:08:04 -0300

    feat: add swagger dependency and enabled it

--- src/main/java/com/orange_evolution_backend/OrangeEvolutionBackendApplication.java
+++ src/main/java/com/orange_evolution_backend/OrangeEvolutionBackendApplication.java
@@ -3,7 +3,10 @@ package com.orange_evolution_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class OrangeEvolutionBackendApplication {
 
 	public static void main(String[] args) {
