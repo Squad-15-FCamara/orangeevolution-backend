15e78873e17d20f282621516c80bf792fe27a77f
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-07 12:43:14 -0300

    feat: entity users

---
+++ src/main/java/com/orange_evolution_backend/entity/User.java
@@ -0,0 +1,38 @@
+package com.orange_evolution_backend.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "userId")
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String actualJob;
+	
+	@Column
+	private Boolean isAdmin;
+	
+}
