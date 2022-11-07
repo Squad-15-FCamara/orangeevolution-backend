8eeba09b8d59b4f7147d59f833408dda5bec9c10
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-07 13:20:19 -0300

    feat: get course by id endpoint add

--- src/main/java/com/orange_evolution_backend/Controller/CourseController.java
+++ src/main/java/com/orange_evolution_backend/Controller/CourseController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,12 @@ import lombok.AllArgsConstructor;
 public class CourseController {
     private CourseService courseService;
 
+    @GetMapping
     public ResponseEntity<List<Course>> getAllCourses(){
         return ResponseEntity.ok(courseService.findAllCourses());
+    }
+    @GetMapping("/{idCourse}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long idCourse){
+        return ResponseEntity.ok(courseService.findCourseByID(idCourse));
     }
 }
