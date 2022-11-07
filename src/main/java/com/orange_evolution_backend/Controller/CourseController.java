6ba3746891f29c59ad15f147822888a62d5e4ba6
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-07 13:35:57 -0300

    feat: create course endpoint add

--- src/main/java/com/orange_evolution_backend/Controller/CourseController.java
+++ src/main/java/com/orange_evolution_backend/Controller/CourseController.java
@@ -2,10 +2,13 @@ package com.orange_evolution_backend.Controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +32,9 @@ public class CourseController {
     public ResponseEntity<Course> getCourseById(@PathVariable Long idCourse){
         return ResponseEntity.ok(courseService.findCourseByID(idCourse));
     }
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course){
+        return new ResponseEntity<Course>(courseService.saveCourse(course), HttpStatus.CREATED);
+    } 
 }
