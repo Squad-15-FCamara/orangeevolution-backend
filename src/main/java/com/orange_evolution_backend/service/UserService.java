cecaf57a130d0af7f3c743a2ede43e74a5437db7
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-07 12:55:28 -0300

    feat: fetch an user by id

--- src/main/java/com/orange_evolution_backend/service/UserService.java
+++ src/main/java/com/orange_evolution_backend/service/UserService.java
@@ -2,7 +2,9 @@ package com.orange_evolution_backend.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.orange_evolution_backend.entity.User;
 import com.orange_evolution_backend.repository.UserRepository;
@@ -20,6 +22,13 @@ public class UserService {
 	public List<User> findAllUsers() {
 		
 		return userRepository.findAll();
+	}
+	
+	public User findUserById(Long userId) {
+		
+		return userRepository.findById(userId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, 
+						"Este usuário não existe!"));
 	}
 	
 }
