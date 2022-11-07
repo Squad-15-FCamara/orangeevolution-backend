e17439f56591fc10e7b4c5cddc45c6a87757f51f
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-07 12:04:38 -0300

    feat: spring boot initial files

---
+++ src/main/java/com/orange_evolution_backend/OrangeEvolutionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.orange_evolution_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeEvolutionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeEvolutionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange_evolution_backend/OrangeEvolutionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange_evolution_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeEvolutionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
