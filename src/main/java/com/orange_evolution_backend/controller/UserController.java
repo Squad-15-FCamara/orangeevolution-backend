92580311aa00902cedf0f1b6654cf23e465bf6f2
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-07 15:12:07 -0300

    feat: added description for users controller HTTP methods on swagger

--- src/main/java/com/orange_evolution_backend/controller/UserController.java
+++ src/main/java/com/orange_evolution_backend/controller/UserController.java
@@ -18,6 +18,7 @@ import com.orange_evolution_backend.entity.User;
 import com.orange_evolution_backend.repository.UserRepository;
 import com.orange_evolution_backend.service.UserService;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 
 @CrossOrigin(origins = "*")
@@ -28,25 +29,29 @@ public class UserController {
 
 	private UserService userService;
 	private UserRepository userRepository;
-
+	
+	@ApiOperation(value = "Fetch all users")
 	@GetMapping
 	public ResponseEntity<List<User>> getAllUsers() {
 
 		return ResponseEntity.ok(userService.findAllUsers());
 	}
-
+	
+	@ApiOperation(value = "Fetch an user by ID")
 	@GetMapping("/{userId}")
 	public ResponseEntity<User> getUserById(@PathVariable Long userId) {
 
 		return ResponseEntity.ok(userService.findUserById(userId));
 	}
-
+	
+	@ApiOperation(value = "Create and save an user")
 	@PostMapping
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 
 		return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.CREATED);
 	}
-
+	
+	@ApiOperation(value = "Update an user")
 	@PutMapping("/{userId}")
 	public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody User user) {
 		if (!userRepository.existsById(userId)) {
@@ -58,7 +63,8 @@ public class UserController {
 
 		return ResponseEntity.ok(user);
 	}
-
+	
+	@ApiOperation(value = "Delete an user")
 	@DeleteMapping("/{userId}")
 	public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
 		if (!userRepository.existsById(userId)) {
