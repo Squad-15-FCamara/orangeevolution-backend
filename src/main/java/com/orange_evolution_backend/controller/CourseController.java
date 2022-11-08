29f9bb399807979629d4a6ee5b1a13f5107802f5
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-08 20:46:41 -0300

    Fix: favorite course

--- src/main/java/com/orange_evolution_backend/controller/CourseController.java
+++ src/main/java/com/orange_evolution_backend/controller/CourseController.java
@@ -88,9 +88,8 @@ public class CourseController {
 
     @ApiOperation(value = "add a favorite course")
     @GetMapping("/favorites/{userId}/{courseId}")
-    public ResponseEntity<Void> addFavoriteCourse(@PathVariable Long userId, @PathVariable Long courseID) {
-        courseService.favoriteCourse(userId, courseID);
-        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    public ResponseEntity<Course> addFavoriteCourse(@PathVariable Long userId, @PathVariable Long courseID) {
+        return ResponseEntity.ok(courseService.favoriteCourse(userId, courseID));
     }
 
     @ApiOperation(value = "Delete a course")
