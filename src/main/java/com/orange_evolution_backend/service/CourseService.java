002fc83976783072f075715e5d2c983f2308c1c8
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-08 19:22:49 -0300

    feat: favorite courses in servicec add

--- src/main/java/com/orange_evolution_backend/service/CourseService.java
+++ src/main/java/com/orange_evolution_backend/service/CourseService.java
@@ -2,13 +2,16 @@ package com.orange_evolution_backend.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.orange_evolution_backend.entity.Course;
+import com.orange_evolution_backend.entity.User;
 import com.orange_evolution_backend.repository.CourseRepository;
+import com.orange_evolution_backend.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -16,6 +19,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseService {
     CourseRepository courseRepository;
+    UserRepository userRepository;
 
     public List<Course> findAllCourses() {
         return courseRepository.findAll();
@@ -65,6 +69,21 @@ public class CourseService {
 
     public List<Course> findCoursesByType(String type){
         return courseRepository.findByType(type);
+    }
+
+    public Course favoriteCourse(Long idUser, Long idCourse){
+        Optional<User> userOpt = userRepository.findById(idUser);
+        Optional<Course> courseOpt = courseRepository.findById(idCourse);
+
+        if(userOpt.isPresent() && courseOpt.isPresent()){
+            Course course = courseOpt.get();
+            course.getUsers().add(userOpt.get());
+
+            return course;
+            
+        }
+
+        return null;
     }
     
     public void deleteCourse(Long courseId) {
