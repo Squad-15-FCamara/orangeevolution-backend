da1e72c2107e51df579bd46b9f9990d0f4308332
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-09 23:12:37 -0300

    fix: Post and get from User and Course working now

--- src/main/java/com/orange_evolution_backend/dto/CourseDTO.java
+++ src/main/java/com/orange_evolution_backend/dto/CourseDTO.java
@@ -2,9 +2,12 @@ package com.orange_evolution_backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+
 public class CourseDTO {
     private Long id;
     private String title;
