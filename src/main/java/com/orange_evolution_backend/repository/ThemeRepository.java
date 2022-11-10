d811d9b1bad9c55004e65b6e44564f2de5edd111
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-10 16:59:24 -0300

    feat: theme table add

---
+++ src/main/java/com/orange_evolution_backend/repository/ThemeRepository.java
@@ -0,0 +1,9 @@
+package com.orange_evolution_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.orange_evolution_backend.entity.Theme;
+
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+    
+}
