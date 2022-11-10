603b13a4479ca55a2490478e88feca8b15eda5d3
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-09 21:20:44 -0300

    Fix/ favorites courses method moved to Statics Service and favorites courses add on endpoint moved to Statics Controller

--- src/main/java/com/orange_evolution_backend/service/StatisticService.java
+++ src/main/java/com/orange_evolution_backend/service/StatisticService.java
@@ -1,6 +1,7 @@
 package com.orange_evolution_backend.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,23 @@ import lombok.AllArgsConstructor;
 public class StatisticService {
     CourseRepository courseRepository;
     UserRepository userRepository;
+
+
+    public Course favoriteCourse(Long idUser, Long idCourse){
+        Optional<User> userOpt = userRepository.findById(idUser);
+        Optional<Course> courseOpt = courseRepository.findById(idCourse);
+
+        if(userOpt.isPresent() && courseOpt.isPresent()){
+            Course course = courseOpt.get();
+            course.getUsers().add(userOpt.get());
+
+            return courseRepository.save(course);
+            
+        }
+
+        return null;
+    }
+    
     
     public List<Course> findFavoritesCoursesByIdUser(Long idUser, Long idcourse){
         User userFavorite = userRepository.findById(idcourse).get();
