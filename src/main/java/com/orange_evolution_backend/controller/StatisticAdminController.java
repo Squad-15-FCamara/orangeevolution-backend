7d9bac2e41f1c3c9f8948978391c0c6856c55a0f
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-10 15:01:53 -0300

    feat: Count user doing a course endpoint in StatisticAdminController add

---
+++ src/main/java/com/orange_evolution_backend/controller/StatisticAdminController.java
@@ -0,0 +1,31 @@
+package com.orange_evolution_backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.orange_evolution_backend.service.StatisticAdminService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/statisticAdmin")
+@AllArgsConstructor
+@Api(description = "Statistic Admin HTTP methods", tags = "Statistics Admin")
+public class StatisticAdminController {
+
+    StatisticAdminService statisticAdminService;
+
+    @ApiOperation(value = "Count Users doing a Course")
+    @GetMapping("/courseDoing/{idCourse}")
+    public ResponseEntity<Long> countUserdoingCourseByIdCourse(@PathVariable Long idCourse){
+        return ResponseEntity.ok(statisticAdminService.counterUserDoing(idCourse));
+    }
+    
+}
