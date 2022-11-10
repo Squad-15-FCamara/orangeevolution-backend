00d007a8caf8fe60e727c861c726fc3b868cc266
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-10 02:46:57 -0300

    fix: dto pattern in StatisticController add

--- src/main/java/com/orange_evolution_backend/controller/StatisticController.java
+++ src/main/java/com/orange_evolution_backend/controller/StatisticController.java
@@ -1,5 +1,9 @@
 package com.orange_evolution_backend.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.orange_evolution_backend.dto.CourseDTO;
 import com.orange_evolution_backend.entity.Course;
 import com.orange_evolution_backend.service.StatisticService;
 
@@ -22,11 +27,25 @@ import lombok.AllArgsConstructor;
 public class StatisticController {
 
     StatisticService statisticService;
+    ModelMapper modelMapper;
 
 
     @ApiOperation(value = "add a favorite course")
     @GetMapping("/favorites/{userId}/{courseId}")
-    public ResponseEntity<Course> addFavoriteCourse(@PathVariable Long userId, @PathVariable Long courseId) {
-        return ResponseEntity.ok(statisticService.favoriteCourse(userId, courseId));
+    public ResponseEntity<CourseDTO> addFavoriteCourse(@PathVariable Long userId, @PathVariable Long courseId) {
+        Course returnCourse = statisticService.favoriteCourse(userId, courseId);
+        return ResponseEntity.ok(convertToDTO(returnCourse));
+    }
+
+    private List<CourseDTO> converToListDTO(List<Course> courses){
+        List<CourseDTO> returnCoursesDTO = new ArrayList<>();
+        courses.forEach(course ->{
+            returnCoursesDTO.add(modelMapper.map(course, CourseDTO.class));
+        });
+        return returnCoursesDTO;
+    }
+
+    private CourseDTO convertToDTO(Course course){
+        return modelMapper.map(course, CourseDTO.class);
     }
 }
