2ca9d88c08009ff9a003cd16dbee0f7b563fb27e
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-12 10:42:43 -0300

    feat: delete all a user's favorites courses

--- src/main/java/com/orange_evolution_backend/entity/Road.java
+++ src/main/java/com/orange_evolution_backend/entity/Road.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
