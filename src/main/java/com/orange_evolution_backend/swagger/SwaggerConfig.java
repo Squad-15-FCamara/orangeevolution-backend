d12ae088842050a68e67a6f2fb7ffcd123ee509a
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-11-11 21:23:14 -0300

    feat: swagger final configuration

--- src/main/java/com/orange_evolution_backend/swagger/SwaggerConfig.java
+++ src/main/java/com/orange_evolution_backend/swagger/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.orange_evolution_backend.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -18,6 +20,15 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.orange_evolution_backend.controller"))
 				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo()).useDefaultResponseMessages(false);
+	}
+	
+	public ApiInfo apiInfo() {
+		
+		return new ApiInfoBuilder().title("Orange Evolution API")
+				.description("Orange Evolution App Models, Controllers and Endpoints")
+				.version("1.0.1")
 				.build();
 	}
 	
