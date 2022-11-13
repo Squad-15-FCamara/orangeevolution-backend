9ee6072f4dd9b4761547aaef060cbc2e9345ba86
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-12 21:26:24 -0300

    feat: errorResponse add

---
+++ src/main/java/com/orange_evolution_backend/exception/ErrorResponse.java
@@ -0,0 +1,31 @@
+package com.orange_evolution_backend.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+    private String stackStrace;
+    private List<ValidationError> errors;    
+
+
+    public void addValidationError(String field, String message){
+        if(Objects.isNull(errors)){
+            errors = new ArrayList<>();
+        }
+        errors.add(new ValidationError(field, message));
+    }
+
+}
