5c7869ca944cdbcfbf0ee78e3ff0527c61a3bc2f
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-12 21:54:01 -0300

    feat: reponse Entity

--- src/main/java/com/orange_evolution_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/orange_evolution_backend/exception/GlobalExceptionHandler.java
@@ -8,16 +8,25 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.orange_evolution_backend.commons.Utils;
+
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
     
     @Value("${enable.trace:#{false}}")
     private boolean enableTracer;
 
-    private ResponseEntity<Object> buildErrorResponse(Exception exception, HttpStatus httpStatus, WebRequest){
-        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), exception.getMessage());
 
-        if(enabletracer)
+    private ResponseEntity<Object> buildErrorResponse(Exception exception, HttpStatus httpStatus, WebRequest webRequest){
+        return buildErrorResponse(exception,exception.getMessage(), httpStatus, webRequest);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception,String message, HttpStatus httpStatus, WebRequest webRequest){
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+
+        if(enableTracer)
+            errorResponse.setStackStrace(Utils.getStackTracer(exception));
+        return ResponseEntity.status(httpStatus).body(errorResponse);
 
     }
 }
