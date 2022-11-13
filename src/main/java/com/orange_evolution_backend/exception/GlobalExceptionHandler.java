1e15cf8b13cbda6a1b5ace75e9ef56b43a75675e
Jean Carlos <jeanfilho.2000@gmail.com>
2022-11-12 21:32:42 -0300

    feat: course no found exception add

--- src/main/java/com/orange_evolution_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/orange_evolution_backend/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.orange_evolution_backend.exception;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
-        
 }
